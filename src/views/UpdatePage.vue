a49ddc1eb092609ead17a8b3e7dbd38c6ea77cea
angrymusic <angrymusic@naver.com>
2023-02-08 17:24:40 +0900

    useRouter, [].filter

--- src/views/UpdatePage.vue
+++ src/views/UpdatePage.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 import axios from "axios";
 let content = ref("");
 let title = ref("");
-
+const router = useRouter();
 const props = defineProps({ id: String });
 onMounted(async () => {
   await axios
@@ -17,11 +18,12 @@ onMounted(async () => {
       console.log("[추가 기존데이터 불러오기 에러] " + err);
     });
 });
-
-const writeContent = (id: string | undefined) => {
-  console.log(id);
-  axios
-    .patch("http://112.220.234.180:18080/api/post/" + id, {
+const backToHome = async () => {
+  await router.push("/contents/" + props.id);
+};
+const writeContent = async () => {
+  await axios
+    .patch("http://112.220.234.180:18080/api/post/" + props.id, {
       title: title.value,
       content: content.value,
     })
@@ -31,16 +33,13 @@ const writeContent = (id: string | undefined) => {
     .catch((err) => {
       console.log("[수정 에러] " + err);
     });
+  backToHome();
 };
 </script>
 
 <template>
-  <RouterLink :to="{ path: `/contents/${props.id}` }"
-    ><button>취소</button></RouterLink
-  >
-  <RouterLink :to="{ path: `/contents/${props.id}` }"
-    ><button @click="writeContent(props.id)">수정</button></RouterLink
-  >
+  <button @click="backToHome">취소</button>
+  <button @click="writeContent">수정</button>
   <form>
     <input type="text" placeholder="title" v-model="title" /><br />
     <textarea placeholder="content" v-model="content"> </textarea>
